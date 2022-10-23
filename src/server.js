1eaceb272b12a5970cf5624b888b655b3fe9eb30
Mônica Camerini <monica.camerini@sequor.com.br>
2022-10-23 16:03:49 -0300

    fix: ports changes

--- src/server.js
+++ src/server.js
@@ -50,6 +50,6 @@ app.get('/', async (req, res) => {
 });
 
 models.initDatabase();
-app.listen(4040);
+app.listen(8080);
 
-console.log('Aplicação rodando na porta 4040');
+console.log('Aplicação rodando na porta 8080');
